7aab29343475c7e8327d2718d90e6dc643112e8f
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-12 15:32:45 +0600

    Adding Authentication Functionality

---
+++ src/Authentication/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBn0mAQX4qNHNAtOBzAlSKkwI-3e40yV-k",
+  authDomain: "ezy-tricket.firebaseapp.com",
+  projectId: "ezy-tricket",
+  storageBucket: "ezy-tricket.firebasestorage.app",
+  messagingSenderId: "210981705818",
+  appId: "1:210981705818:web:9292921131ec37dad66063",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
